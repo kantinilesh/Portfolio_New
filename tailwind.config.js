3dd32f432070eff1b6ee4a629164cf573444d3aa
Nilesh <61074524+kantinilesh@users.noreply.github.com>
2025-02-28 01:02:40 +0530

    updated tailwind config js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        montserrat: ['var(--font-montserrat)', 'sans-serif'], // Fallback to sans-serif
+        montserrat: ['var(--font-montserrat)', 'sans-serif'],
+        'luckiest-guy': ['var(--font-luckiest-guy)', 'sans-serif'],
       },
       backgroundImage: {
         'cloud-background': "url('/images/background-clouds.jpg')",
